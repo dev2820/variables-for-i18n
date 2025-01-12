35d6f49cf78ad7cfd022a1df9832d4b36e490e3a
dev2820 <second28200@naver.com>
2025-01-12 22:34:05 +0900

    chore: Plugin Development Env setup

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,1 +1,16 @@
-figma.showUI(__html__);
+function showPluginUI() {
+  figma.showUI(__html__);
+}
+
+function setup() {
+  figma.ui.onmessage = (msg) => {
+    // handle msg
+  };
+}
+
+function main() {
+  setup();
+  showPluginUI();
+}
+
+main();
